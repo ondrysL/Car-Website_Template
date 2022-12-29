ba34cd624ef55ab9fc2cdeb86af95474d819034b
ondrysL <linart.ondrej@gmail.com>
2022-12-29 15:52:05 +0100

    responsiziv e navbar

---
+++ main.js
@@ -0,0 +1,24 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToogle = document.getElementById('nav-toogle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToogle){
+    navToogle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
